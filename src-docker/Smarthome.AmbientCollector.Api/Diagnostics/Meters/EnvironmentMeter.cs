6e925aada1139027a6b28d96bc398fa65d15d600
ludwig <blush-chewy-strike@duck.com>
2026-03-27 20:38:52 +0100

    #131 rework meter

--- src-docker/Smarthome.AmbientCollector.Api/Diagnostics/Meters/EnvironmentMeter.cs
+++ src-docker/Smarthome.AmbientCollector.Api/Diagnostics/Meters/EnvironmentMeter.cs
@@ -1,33 +1,62 @@
-﻿using System.Diagnostics.Metrics;
+﻿using System.Collections.Concurrent;
+using System.Diagnostics.Metrics;
 
 namespace Smarthome.AmbientCollector.Api.Diagnostics.Meters
 {
-	public static class EnvironmentMeter
-	{
-		public static string Name => "EnvironmentSensor.Readings";
-		private static readonly Meter _meter = new( Name, "1.0" );
-		private static readonly Gauge<double> _temperatureGauge;
-		private static readonly Gauge<double> _humidityGauge;
-		private static readonly Gauge<double> _pressureGauge;
+    public static class EnvironmentMeter
+    {
+        public static string Name => "EnvironmentSensor.Readings";
+        private static readonly Meter Meter = new(Name, "1.0");
 
-		static EnvironmentMeter()
-		{
-			// Initialize the metrics
-			_temperatureGauge = _meter.CreateGauge<double>( "envsensor.temperature", "degC", "Current temperature in Celsius" );
-			_humidityGauge = _meter.CreateGauge<double>( "envsensor.humidity", "%", "Current humidity in %" );
-			_pressureGauge = _meter.CreateGauge<double>( "envsensor.pressure", "hPa", "Current ambient pressure in hPa" );
-		}
+        private static readonly ConcurrentDictionary<string, Measurement<double>> TemperatureMeasurements = [];
+        private static readonly ConcurrentDictionary<string, Measurement<double>> HumidityMeasurements = [];
+        private static readonly ConcurrentDictionary<string, Measurement<double>> PressureMeasurements = [];
 
-		public static void Update( double temperature, double humidity, double pressure, string location, string sensorModel )
-		{
-			var tags = new KeyValuePair<string, object?>[]
-			{
-				new("location", location),
-				new("sensor_model", sensorModel)
-			};
-			_temperatureGauge.Record( temperature, tags );
-			_humidityGauge.Record( humidity, tags );
-			_pressureGauge.Record( pressure, tags );
-		}
-	}
+        private static readonly ObservableGauge<double> TemperatureGauge;
+        private static readonly ObservableGauge<double> HumidityGauge;
+        private static readonly ObservableGauge<double> PressureGauge;
+
+        static EnvironmentMeter()
+        {
+            // Initialize the metrics
+            TemperatureGauge = Meter.CreateObservableGauge<double>(
+                "envsensor.temperature",
+                () => TemperatureMeasurements.Values,
+                "degC",
+                "Current temperature in Celsius");
+            
+            
+            HumidityGauge = Meter.CreateObservableGauge<double>(
+                "envsensor.humidity",
+                () => HumidityMeasurements.Values,
+                "%",
+                "Current humidity in %");
+            
+            PressureGauge = Meter.CreateObservableGauge<double>(
+                "envsensor.pressure",
+                () => PressureMeasurements.Values,
+                "hPa",
+                "Current ambient pressure in hPa");
+        }
+
+        public static void Update( double temperature, double humidity, double pressure, string location, string sensorModel )
+        {
+            var tags = new KeyValuePair<string, object?>[]
+            {
+                new("sensor.location", location),
+                new("sensor.model", sensorModel),
+            };
+
+            TemperatureMeasurements.TryAdd(location, new Measurement<double>(temperature, tags));
+            HumidityMeasurements.TryAdd(location, new Measurement<double>(humidity, tags));
+            PressureMeasurements.TryAdd(location, new Measurement<double>(pressure, tags));
+        }
+
+        public static void TryRemoveSensor(string location)
+        {
+            TemperatureMeasurements.TryRemove(location, out _);
+            HumidityMeasurements.TryRemove(location, out _);
+            PressureMeasurements.TryRemove(location, out _);
+        }
+    }
 }
